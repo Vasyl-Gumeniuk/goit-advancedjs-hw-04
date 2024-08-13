cce7ce4be87050bed7aca2bb292618a2f8fe0c13
Vasyl-Gumeniuk <vasyl.gumeniuk@gmail.com>
2024-08-13 16:22:34 +0300

    final version

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,20 @@
+import axios from 'axios';
+
 const API_KEY = '25798215-b5224b890c985f6c53280bcb2';
 const URL = 'https://pixabay.com/api/?';
 
-export function fetchImages(query) {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+export function fetchImages(query, currentPage) {
+  const axiosConfigs = {
+    params: {
+      key: API_KEY,
+      q: query,
+      page: currentPage,
+      per_page: 15,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  };
 
-  return fetch(`${URL}${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios.get(`${URL}`, axiosConfigs);
 }
